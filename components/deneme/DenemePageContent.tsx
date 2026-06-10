9077a2b83639d8de17a657d3d5eceab612a5b768
bedirhanorseloglu <bedirhan_6175@hotmail.com>
2026-06-11 01:34:49 +0300

    fix: type error in DenemePageContent

--- components/deneme/DenemePageContent.tsx
+++ components/deneme/DenemePageContent.tsx
@@ -348,8 +348,9 @@ export default function DenemePageContent() {
                         name: editing.name,
                         date: editing.date,
                         publisher: editing.publisher,
-                        note: editing.note,
                         scores: editing.scores,
+                        examType: editing.examType,
+                        bransSubjectId: editing.bransSubjectId,
                       }
                     : undefined
                 }
